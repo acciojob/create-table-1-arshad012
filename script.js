1a3fbb6250b994293ec101e0df9b5a290b9a4a9b
arshad012 <forarshad99@gmail.com>
2024-10-25 15:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const sampleTable = document.getElementById('sampleTable');
+
+	const tr = document.createElement('tr');
+	const td1 = document.createElement('td');
+	td1.innerText = 'New Cell1';
+	const td2 = document.createElement('td');
+	td2.innerText = 'New Cell2';
+
+	tr.append(td1, td2);
+
+	sampleTable.append(tr);
 }
